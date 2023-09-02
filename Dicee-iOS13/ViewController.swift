df5ac178e58eefa7f58f11db4f0191900382422d
marufkhan09 <maruf1609@gmail.com>
2023-09-02 21:24:51 +0600

    button added to roll dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,11 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var img1: UIImageView!
+    @IBOutlet weak var img2: UIImageView!
+    let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    var left = 0
+    var right = 5
+  func   reset(){
+      left = 0
+      right = 5
 
+    }
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func rollButtonPressed(_ sender: Any) {
+
+      
+        if( right >= 0 && left < 6 ){
+            img1.image = diceArray[left]
+            img2.image = diceArray[right]
+            left = left + 1
+            right = right - 1
+            print("left:::\(left)")
+            print("right::::\(right)")
+        }
+        else {
+            print("reset hoise")
+            reset()
+        }
+
+        
+      
+    }
+
 
 
 }
